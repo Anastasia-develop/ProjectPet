9b4ddd0ec6a387d7143db18561b2d740beb30cd3
Anastasia-develop <kozl0vaan4s@yandex.ru>
2025-08-26 16:39:40 +0300

    throw strictMode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
-import { StrictMode } from 'react';
 
 export const App = () => {
   return (
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
+    // <StrictMode>
+    <RouterProvider router={router} />
+    // </StrictMode>
   );
 };
