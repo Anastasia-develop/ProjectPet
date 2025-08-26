aa6b62069653ceefe3ad9355e96c258a3a0a8c01
Anastasia-develop <kozl0vaan4s@yandex.ru>
2025-08-26 15:44:49 +0300

    Base-url

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,10 +1,13 @@
-import { createBrowserRouter } from "react-router";
-import { BaseLayout } from "../layout/base-layout";
+import { createBrowserRouter } from 'react-router';
+import { BaseLayout } from '../layout/base-layout';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
     {
-        path: '/',
-        element: <BaseLayout />,
-        children: []
-    }
-])
+      path: '/',
+      element: <BaseLayout />,
+      children: [],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
